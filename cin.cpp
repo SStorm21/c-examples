2b8869b3273cf265d4415aac16cfc32741d40670
Storm⛈️ <176233500+SStorm21@users.noreply.github.com>
2026-02-07 17:41:32 +0300

    Add basic user input program in cin.cpp

---
+++ cin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string name = "none";
+  int age = 0;
+  
+  cout<<"insert ur name:> ";
+  cin>>name;
+  cout<<"insert ur age:> ";
+  cin>>age;
+  
+  cout<<"hello there "<<name<<" & "<<age; 
+
+  return 0;
+}
